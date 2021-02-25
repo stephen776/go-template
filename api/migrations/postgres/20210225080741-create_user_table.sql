4e543c1f8a7a2e6e36e9bb0f33822ac281dc40e1
Stephen Jacobs <sjacobs@truefit.io>
2021-02-25 08:39:53 -0500

    check in migrations

---
+++ api/migrations/postgres/20210225080741-create_user_table.sql
@@ -0,0 +1,12 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS users (
+  id         SERIAL       NOT NULL,
+  email      VARCHAR(256) NOT NULL,
+  first_name VARCHAR(50)  NOT NULL,
+  lasst_name VARCHAR(50)  NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- +migrate Down
+DROP TABLE users;
